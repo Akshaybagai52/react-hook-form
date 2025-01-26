385ba33770072af065e3b2b86278613cf232bb27
Akshaybagai52 <akshaybagai52@gmail.com>
2025-01-26 21:55:20 +0530

    feat: added register to inputs

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,11 @@
 import { useState } from 'react'
 import './App.css'
 import YoutubeForm from './components/YoutubeForm'
+import { useForm } from 'react-hook-form'
 
 function App() {
+  const form = useForm();
+  
 
   return (
     <div>
--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -1,11 +1,18 @@
 import React from "react";
+import { useForm } from "react-hook-form";
 
 const YoutubeForm = () => {
+  const form = useForm();
+  const { register } = form;
   return (
     <div>
       <form action="">
         <label htmlFor="username">Username</label>
-        <input type="text" name="username" id="username" />
+        <input
+          type="text"
+          id="username"
+          {...register("username")}
+          />
         <label htmlFor="email">E-mail</label>
         <input type="text" name="email" id="email" />
         <label htmlFor="password">Password</label>
