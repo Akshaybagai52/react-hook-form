c3db38164301ac75c48acd90824d15846d65dad7
Akshaybagai52 <akshaybagai52@gmail.com>
2025-01-26 22:09:40 +0530

    feat: added devtools and onsubmit

--- src/components/YoutubeForm.tsx
+++ src/components/YoutubeForm.tsx
@@ -1,24 +1,31 @@
 import React from "react";
 import { useForm } from "react-hook-form";
+import { DevTool } from "@hookform/devtools";
+
+type FormValues = {
+  username: string;
+  email: string;
+  password: string;
+};
 
 const YoutubeForm = () => {
-  const form = useForm();
-  const { register } = form;
+  const form = useForm<FormValues>();
+  const { register, control, handleSubmit } = form;
+  const onSubmit = (data: FormValues) => {
+    console.log("Form Submitted", data);
+  };
   return (
     <div>
-      <form action="">
+      <form action="" onSubmit={handleSubmit(onSubmit)}>
         <label htmlFor="username">Username</label>
-        <input
-          type="text"
-          id="username"
-          {...register("username")}
-          />
+        <input type="text" id="username" {...register("username")} />
         <label htmlFor="email">E-mail</label>
-        <input type="text" name="email" id="email" />
+        <input type="text" {...register("email")} id="email" />
         <label htmlFor="password">Password</label>
-        <input type="password" name="password" id="password" />
+        <input type="password" {...register("password")} id="password" />
 
         <button>Submit</button>
+        <DevTool control={control} />
       </form>
     </div>
   );
